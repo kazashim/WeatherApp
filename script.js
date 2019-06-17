9205c245a1200505eb6ee3160a5a713bc8e8c09d
blessedjasonmwanza <mwanzabj@gmail.com>
2019-06-17 12:21:25 +0200

    Fixed error modal closing enability

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 $(function(){
-    $('.close').click(function(){
-        alert('clicked');
-        $('#error-cover').fadeOut();
-    });
+
     $('a').bind('copy paste cut',function(e){
         e.preventDefault();
     });
